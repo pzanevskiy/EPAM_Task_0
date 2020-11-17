2016dd86e08e74d9ba4e98a35a503352f86daac1
p.zanevskiy <pavel.zanevsky15@gmail.com>
2020-11-17 23:52:43 +0300

    добавлены некоторы функции

--- EPAM_Task_0/MediaPlayer.cs
+++ EPAM_Task_0/MediaPlayer.cs
@@ -17,5 +17,43 @@ namespace EPAM_Task_0
                 Play(item);
             }
         }
+        public void Pause(MediaFile mediaFile)
+        {
+
+        }
+
+        public void Pause(Playlist playlist)
+        {
+
+        }
+
+        public void Stop(MediaFile mediaFile)
+        {
+
+        }
+
+        public void Stop(Playlist playlist)
+        {
+
+        }
+        public void Next(MediaFile mediaFile)
+        {
+
+        }
+
+        public void Next(Playlist playlist)
+        {
+
+        }
+
+        public void Previous(MediaFile mediaFile)
+        {
+
+        }
+
+        public void Previous(Playlist playlist)
+        {
+
+        }
     }
 }
